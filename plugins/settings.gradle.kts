62b6e2535204887d7ddf5d84a1f4fed6eac7f8b1
ArenHansy <aren.hansy@gmail.com>
2025-08-13 17:04:43 +0900

    add 'plugins'

---
+++ plugins/settings.gradle.kts
@@ -0,0 +1,24 @@
+rootProject.name = "gradle-plugins"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("resources/versions/build-plugins.toml"))
+        }
+    }
+}
+
+include(":convention")
